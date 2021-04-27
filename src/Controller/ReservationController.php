0a3e58d2c0faddb3be68e341f6043259e70fd72f
ValouNatsuki <66723597+ValDev12@users.noreply.github.com>
2021-04-27 16:09:48 +0200

    controller supprimer par erreur

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Reservation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReservationController extends AbstractController
+{
+    /**
+     * @Route("/reservation", name="reservation")
+     */
+    public function index(): Response
+    {
+        return $this->render('reservation/index.html.twig', [
+            'controller_name' => 'ReservationController',
+        ]);
+    }
+
+    /**
+     * @Route("/reservation/list", name="list_r")
+     */
+    public function list(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $res = $this->getDoctrine()->getRepository(Reservation::class)->findAll();
+        return $this->render('reservation/AfficheReservation.html.twig', [
+            'reservations' => $res,
+        ]);
+    }
+
+
+     /**
+     * @Route("/reservation/add/{id}", name="add_r")
+     */
+    public function add(Request $req): Response
+    {
+        $uneReservation= new Reservation();
+        $entityManager = $this->getDoctrine()->getManager();
+        $users  = $this->getDoctrine()->getRepository(Utilisateur::class)->findAll();
+        for ($i=0;$i<count($users);$i++)
+        {
+            if(in_array($id, $users[$i]->getId()))
+            {
+                $leCreateur=$users[$i];
+                $i=count($users);
+            }
+        }
+        $uneReservation->setCreateur($leCreateur);
+        $usersAll  = $this->getDoctrine()->getRepository(Utilisateur::class)->findAll();
+        $form=$this->createForm(ReservationType::class ,$uneReservation,['utilisateurs'=>$usersAll,]);
+
+        $form-> handleRequest($req);
+        if($form->isSubmitted())
+        {
+           $entityManager->persist($uneReservation);
+            $entityManager->flush();
+            $response=$this->redirectToRoute('list_r');
+        }
+        else
+        {
+            $response= $this->render('reservation/AjoutReservation.html.twig',['formulaire' => $form->createView(), ]);
+        }   
+             return $response;
+    }
+}
