2c08b7754e0ef7e912e4f415f34c72eb46d3dd15
Jason-prog1 <72190216+Jason-prog1@users.noreply.github.com>
2021-05-04 12:08:13 +0200

    miseAJour

---
+++ migrations/Version20210504100713.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210504100713 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, createur_id INT DEFAULT NULL, salle_id INT DEFAULT NULL, date_d DATETIME NOT NULL, date_f DATETIME NOT NULL, INDEX IDX_42C8495573A201E5 (createur_id), INDEX IDX_42C84955DC304035 (salle_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reservation_utilisateur (reservation_id INT NOT NULL, utilisateur_id INT NOT NULL, INDEX IDX_D170BEEFB83297E7 (reservation_id), INDEX IDX_D170BEEFFB88E14F (utilisateur_id), PRIMARY KEY(reservation_id, utilisateur_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE salle (id INT AUTO_INCREMENT NOT NULL, type_id INT DEFAULT NULL, libelle VARCHAR(255) NOT NULL, nb_places INT NOT NULL, INDEX IDX_4E977E5CC54C8C93 (type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE salle_service (salle_id INT NOT NULL, service_id INT NOT NULL, INDEX IDX_CCABF683DC304035 (salle_id), INDEX IDX_CCABF683ED5CA9E6 (service_id), PRIMARY KEY(salle_id, service_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, roles_id INT DEFAULT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) NOT NULL, identifiant VARCHAR(50) NOT NULL, mdp VARCHAR(50) NOT NULL, INDEX IDX_1D1C63B338C751C4 (roles_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C8495573A201E5 FOREIGN KEY (createur_id) REFERENCES utilisateur (id)');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955DC304035 FOREIGN KEY (salle_id) REFERENCES salle (id)');
+        $this->addSql('ALTER TABLE reservation_utilisateur ADD CONSTRAINT FK_D170BEEFB83297E7 FOREIGN KEY (reservation_id) REFERENCES reservation (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE reservation_utilisateur ADD CONSTRAINT FK_D170BEEFFB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE salle ADD CONSTRAINT FK_4E977E5CC54C8C93 FOREIGN KEY (type_id) REFERENCES type (id)');
+        $this->addSql('ALTER TABLE salle_service ADD CONSTRAINT FK_CCABF683DC304035 FOREIGN KEY (salle_id) REFERENCES salle (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE salle_service ADD CONSTRAINT FK_CCABF683ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE utilisateur ADD CONSTRAINT FK_1D1C63B338C751C4 FOREIGN KEY (roles_id) REFERENCES role (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reservation_utilisateur DROP FOREIGN KEY FK_D170BEEFB83297E7');
+        $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B338C751C4');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955DC304035');
+        $this->addSql('ALTER TABLE salle_service DROP FOREIGN KEY FK_CCABF683DC304035');
+        $this->addSql('ALTER TABLE salle_service DROP FOREIGN KEY FK_CCABF683ED5CA9E6');
+        $this->addSql('ALTER TABLE salle DROP FOREIGN KEY FK_4E977E5CC54C8C93');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C8495573A201E5');
+        $this->addSql('ALTER TABLE reservation_utilisateur DROP FOREIGN KEY FK_D170BEEFFB88E14F');
+        $this->addSql('DROP TABLE reservation');
+        $this->addSql('DROP TABLE reservation_utilisateur');
+        $this->addSql('DROP TABLE role');
+        $this->addSql('DROP TABLE salle');
+        $this->addSql('DROP TABLE salle_service');
+        $this->addSql('DROP TABLE service');
+        $this->addSql('DROP TABLE type');
+        $this->addSql('DROP TABLE utilisateur');
+    }
+}
